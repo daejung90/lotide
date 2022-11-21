c86037b2180d775cb4a37f583db3c25c5758e2e2
daejung90 <djung.ca@gmail.com>
2022-11-21 17:42:41 -0500

    refactoring functions using methods of export and require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,6 @@
-const assertEqual = function(actual, expected) {
+const eqArrays = require('./eqArrays')
+
+const assertArrayEqual = function(actual, expected) {
     if (eqArrays(actual,expected)) {
       console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
     } else {
@@ -15,23 +17,28 @@ const assertEqual = function(actual, expected) {
     
   // };
 
-  const eqArrays = function(arrOne, arrTwo) {
+  // const eqArrays = function(arrOne, arrTwo) {
   
-    if(arrOne.length !== arrTwo.length){
-      return false;
-    }
+  //   if(arrOne.length !== arrTwo.length){
+  //     return false;
+  //   }
   
-    for (let i = 0; i < arrOne.length; i++) {
-    if(Array.isArray(arrOne[i]) && Array.isArray(arrTwo[i])){
-      if(!eqArrays(arrOne[i], arrTwo[i])){
-        return false
-      }
-      }else if(arrOne[i] !== arrTwo[i]){
-        return false
-      }
-    }
+  //   for (let i = 0; i < arrOne.length; i++) {
+  //   if(Array.isArray(arrOne[i]) && Array.isArray(arrTwo[i])){
+  //     if(!eqArrays(arrOne[i], arrTwo[i])){
+  //       return false
+  //     }
+  //     }else if(arrOne[i] !== arrTwo[i]){
+  //       return false
+  //     }
+  //   }
     
-    return true
-  };
+  //   return true
+  // };
+
+  
+  module.exports = assertArrayEqual;
+
+  
 
   
